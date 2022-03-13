f74f45aa4d01bedfa555b741e8ff6078717c0969
Anastasiia Duplishcheva <a.duplishcheva@gmail.com>
2022-03-13 22:49:51 +0300

    header section

--- src/components/app-header/app-header.jsx
+++ src/components/app-header/app-header.jsx
@@ -1,10 +1,41 @@
 import React from 'react';
 import appHeaderStyles from './app-header.module.css';
+import { BurgerIcon, ListIcon, Logo, ProfileIcon } from '@ya.praktikum/react-developer-burger-ui-components';
 
 class AppHeader extends React.Component {
   render() {
     return (
-      
+      <div style={{ backgroundColor: '#1C1C21' }} className = {appHeaderStyles.header}>
+        <nav className = {appHeaderStyles.menu}>
+          <ul className = {appHeaderStyles.list}>
+            <li className={`${appHeaderStyles.header__item} ml-0 mr-2 mb-4 mt-4 pl-5 pr-5 pb-4 pt-4`}>
+              <div className="ml-0 mr-2 mb-0 mt-0">
+                <BurgerIcon type="primary"/>
+              </div>
+              <p className="text text_type_main-default">
+                Конструктор
+              </p>
+            </li>
+            <li className={`${appHeaderStyles.header__item} ml-0 mr-0 mb-4 mt-4 pl-5 pr-5 pb-4 pt-4`}>
+              <div className="ml-0 mr-2 mb-0 mt-0">
+                <ListIcon type="primary"/>
+              </div>
+              <p className="text text_type_main-default">
+                Лента заказов
+              </p>
+            </li>
+          </ul>
+        </nav>  
+        <Logo />
+        <div className={`${appHeaderStyles.header__item} ml-0 mr-0 mb-4 mt-4 pl-5 pr-5 pb-4 pt-4`}>
+          <div className="ml-0 mr-2 mb-0 mt-0">
+            <ProfileIcon type="primary"/>
+          </div>
+          <p className="text text_type_main-default">
+            Личный кабинет
+          </p>
+        </div>  
+      </div>
     )
   }
 }
--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
-import './App.css';
+import appStyles from './app.module.css';
+import AppHeader from '../app-header/app-header.jsx';
 
 function App() {
   return (
-    <div>
-      Hello!
+    <div className={appStyles.body}>
+      <AppHeader />
     </div>
   );
 }
