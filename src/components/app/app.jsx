e76f75d99ae1335e20965c073b7353b67d43ac93
Anastasiia Duplishcheva <a.duplishcheva@gmail.com>
2022-03-19 22:41:11 +0300

    add fetch data

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,17 +1,44 @@
+import {React, useState, useEffect} from 'react';
 import appStyles from './app.module.css';
 import AppHeader from '../app-header/app-header.jsx';
 import BurgerIngredients from '../burger-ingredients/burger-ingredients.jsx';
 import BurgerConstructor from '../burger-constructor/burger-constructor.jsx';
-import data from '../../utils/data.js';
 
 function App() {
+  const [ingredients, setIngredients] = useState({
+    data: null,
+    isLoading: false,
+    hasError: false,
+  });
+
+  const { data, isLoading, hasError } = ingredients;
+  
+  useEffect(()=>{
+    setIngredients({...ingredients, hasError: false, isLoading: true});
+    getIngredients();
+  }, []);
+
+  const getIngredients = async() => {
+    fetch('https://norma.nomoreparties.space/api/ingredients')
+      .then(res => res.json())
+      .then(data => setIngredients({ ...ingredients, data: data, isLoading: false }))
+      .catch(e => {
+        setIngredients({ ...ingredients, hasError: true, isLoading: false });
+      });
+  }
+
   return (
     <div className={`${appStyles.body} mt-10 mb-10`}>
       <AppHeader />
+      {isLoading && "Загрузка ..."}
+      {hasError && "Ошибка"}
+      {!isLoading && !hasError && data &&
       <main className={appStyles.main}>
-        <BurgerIngredients ingredients={data}/>
-        <BurgerConstructor ingredients={data}/>
+        <BurgerIngredients ingredients={data.data}/>
+        <BurgerConstructor ingredients={data.data}/>
       </main>
+      }
+      {console.log(data)}
     </div>
   );
 }
