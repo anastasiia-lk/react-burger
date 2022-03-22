a26da297358eeef75f1d6e246aa092085fbcb209
Anastasiia Duplishcheva <a.duplishcheva@gmail.com>
2022-03-22 11:31:40 +0300

    add overlay click

--- src/components/modal-overlay/modal-overlay.jsx
+++ src/components/modal-overlay/modal-overlay.jsx
@@ -1,8 +1,12 @@
 import modalOverlayStyles from './modal-overlay.module.css';
 
 function ModalOverlay (props) {
+  function handleClose (evt) {
+    if (evt.target === evt.currentTarget) props.closeModal()
+  }
+
   return (
-    <div className={modalOverlayStyles['modal-overlay']} onClick={props.closeModal}>
+    <div className={modalOverlayStyles['modal-overlay']} onClick={handleClose}>
       {props.children}
     </div>
   )
