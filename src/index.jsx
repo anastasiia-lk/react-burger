c03296f921792f3184415697c93fe80bb76b825d
Anastasiia Duplishcheva <a.duplishcheva@gmail.com>
2022-04-12 21:17:05 +0300

    add store.js

--- src/index.jsx
+++ src/index.jsx
@@ -3,20 +3,8 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/app/app.jsx';
 import reportWebVitals from './reportWebVitals';
-import { rootReducer } from './services/reducers/rootReducer';
 import { Provider } from 'react-redux';
-
-import { compose, createStore, applyMiddleware } from 'redux';								
-import thunk from 'redux-thunk';								
-								
-const composeEnhancers =								
-  typeof window === 'object' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__								
-    ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({})								
-    : compose;								
-								
-const enhancer = composeEnhancers(applyMiddleware(thunk));								
-const store = createStore(rootReducer, enhancer); 								
-
+import { store } from './services/store';							
 
 ReactDOM.render(
   <React.StrictMode>
