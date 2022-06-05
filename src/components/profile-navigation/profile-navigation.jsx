9c66077e0f38bf0873efac0ce78073f61aeeecd4
Anastasiia Duplishcheva <a.duplishcheva@gmail.com>
2022-06-05 11:42:41 +0300

    add user logout

--- src/components/profile-navigation/profile-navigation.jsx
+++ src/components/profile-navigation/profile-navigation.jsx
@@ -3,13 +3,29 @@ import {CheckMarkIcon, Input, Button} from '@ya.praktikum/react-developer-burger
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import { profileFormConfig as config } from '../../utils/data';
+import { useDispatch } from 'react-redux';
+ import { useCallback } from 'react';
+import { signOutUser } from '../../services/actions/user';
 
 export default function ProfileNavigation() {
+  const dispatch = useDispatch()
+  const onClickHandler = useCallback(
+    () => {
+      dispatch(signOutUser());
+    },
+    [dispatch],
+  )
   return (
     <div className={`${profileNavigation[`container`]}`}>
       {config.navigationArr.map((title) => 
         <div key={title} className={`${profileNavigation[`nav-item`]} mb-6`}>
-          <Link to="/register" className="text text_type_main-medium">{title}</Link>
+          {(title === 'Профиль'|| title === 'История заказов') &&
+            <Link to="/register" className="text text_type_main-medium">
+              {title}
+            </Link>}
+          {title === 'Выход' && 
+            <button className={`${profileNavigation[`btn`]} text text_type_main-medium`} onClick={onClickHandler}>{title}
+            </button>}
         </div>
       )}
     </div>
