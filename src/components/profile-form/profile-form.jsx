f1e73b06a0e54990da3b3293ea6a955b90ae4738
Anastasiia Duplishcheva <a.duplishcheva@gmail.com>
2022-06-11 15:04:49 +0300

    add profile form inputs

--- src/components/profile-form/profile-form.jsx
+++ src/components/profile-form/profile-form.jsx
@@ -5,7 +5,8 @@ import { Link } from 'react-router-dom';
 import { profileFormConfig as config } from '../../utils/data';
 import { useDispatch, useSelector } from 'react-redux';
 import { updateUserInfo, getUserInfo } from '../../services/actions/user';
-import {useState, useMemo, useCallback, useEffect} from 'react';
+import {useState, useMemo, useCallback, useEffect, useRef} from 'react';
+import { PASSWORD, TEXT } from '../../utils/data'
 
 export default function ProfileForm() {
   const { name, email } = useSelector((store) => store.user.user);
@@ -18,12 +19,48 @@ export default function ProfileForm() {
   const [nameValue, setNameValue] = useState(name);
   const [emailValue, setEmailValue] = useState(email);
   const [passwordValue, setPasswordValue] = useState('');
+  const [isChange, setIsChange] = useState(false);
+  const [passInputType, setPassInputType] = useState(PASSWORD);
+
+  const nameInputRef = useRef(null);
+  const emailInputRef = useRef(null);
+  const passInputRef = useRef(null);
 
   const body = useMemo(() => ({
     name: nameValue,
     email: emailValue,
     password: passwordValue
   }), [nameValue, emailValue, passwordValue])
+
+  const onChangeHandler = useCallback((e, setFn) => {
+    setFn(e.target.value);
+    setIsChange(true);
+  }, []);
+
+  const onBlurHandler = useCallback((e) => {
+    e.target.disabled = true;
+  }, []);
+
+  const onIconClickHandler = useCallback((ref) => {
+    ref.current.disabled = false;
+    ref.current.focus();
+  }, []);
+
+  const passOnIconClickHandler = useCallback(
+    (ref) => {
+      onIconClickHandler(ref);
+      setPassInputType(TEXT);
+    },
+    [onIconClickHandler]
+  );
+
+  const passOnBlurHandler = useCallback(
+    (e) => {
+      onBlurHandler(e);
+      setPassInputType(PASSWORD);
+    },
+    [onBlurHandler]
+  );
 
   const submitHandler = useCallback((e, body) => {
     e.preventDefault();
@@ -36,6 +73,7 @@ export default function ProfileForm() {
       setNameValue(name);
       setEmailValue(email);
       setPasswordValue('');
+      setIsChange(false);
     },
     [email, name]
   );
@@ -43,44 +81,56 @@ export default function ProfileForm() {
   return (
     <form className={`${form.container}`} onSubmit={(e) => submitHandler(e, body)}>
         <div className ='mb-6'>
-          <Input 
+          <Input
+            ref={nameInputRef}
             type = {`${config.inputsArr[0].type}`}
             placeholder = {`${config.inputsArr[0].placeholder}`}
             icon = {`${config.inputsArr[0].icon}`}
             name = {`${config.inputsArr[0].name}`}
             value = {nameValue}
-            onChange={e => setNameValue(e.target.value)}
+            onChange={(e) => onChangeHandler(e, setNameValue)}
+            onIconClick={() => onIconClickHandler(nameInputRef)}
+            onBlur={(e) => onBlurHandler(e)}
+            disabled
             // onChange={e => setValue(e.target.value)}
           />
         </div>
         <div className ='mb-6'>
-          <Input 
+          <Input
+            ref={emailInputRef} 
             type = {`${config.inputsArr[1].type}`}
             placeholder = {`${config.inputsArr[1].placeholder}`}
             icon = {`${config.inputsArr[1].icon}`}
             name = {`${config.inputsArr[1].name}`}
             value = {emailValue}
-            onChange={e => setEmailValue(e.target.value)}
+            onChange={(e) => onChangeHandler(e, setEmailValue)}
+            onIconClick={() => onIconClickHandler(emailInputRef)}
+            onBlur={(e) => onBlurHandler(e)}
+            disabled
             // onChange={e => setValue(e.target.value)}
           />
         </div>
         <div className ='mb-6'>
-          <Input 
-            type = {`${config.inputsArr[2].type}`}
+          <Input
+            ref={passInputRef} 
+            type = {passInputType}
             placeholder = {`${config.inputsArr[2].placeholder}`}
             icon = {`${config.inputsArr[2].icon}`}
             name = {`${config.inputsArr[2].name}`}
             value = {passwordValue}
-            onChange={e => setPasswordValue(e.target.value)}
+            onChange={(e) => onChangeHandler(e, setPasswordValue)}
+            onIconClick={() => passOnIconClickHandler(passInputRef)}
+            onBlur={(e) => passOnBlurHandler(e)}
+            disabled
             // onChange={e => setValue(e.target.value)}
           />
         </div>
-        <div className={`${form.wrapper}`}>
+        {isChange && (<div className={`${form.wrapper}`}>
         <Button type="secondary" onClick={(e) => cancelHandler(e)}>
             Отмена
         </Button>
         <Button type="primary">Сохранить</Button>
-        </div>
+        </div>)}
     </form>
   )
 }
