fb5894dddde1852fa25c0dae0f718e6eb7220d61
Anastasiia Duplishcheva <a.duplishcheva@gmail.com>
2022-07-23 11:00:25 +0300

    add order action, order thunk

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './components/app/app.jsx';
+import App from './components/app/app.tsx';
 import reportWebVitals from './reportWebVitals';
 import { Provider } from 'react-redux';
 import { store } from './services/store';
