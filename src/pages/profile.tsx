568bb7425d120c09c9010b31f6a295049bc2949e
Anastasiia Duplishcheva <a.duplishcheva@gmail.com>
2022-07-23 12:15:37 +0300

    add profile page

--- src/pages/profile.tsx
+++ src/pages/profile.tsx
@@ -1,10 +1,11 @@
 import { Outlet } from 'react-router-dom';
+import React, { FC } from 'react';
 
 import ProfileNavigation from '../components/profile-navigation/profile-navigation';
 
 import profile from './profile.module.css'
 
-export function Profile() {
+export const Profile: FC = () => {
 
   return (
     <div className={`${profile[`container`]} mt-30 ml-5`}>
